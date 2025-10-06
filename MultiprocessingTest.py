b1c0e0f0e06cc2e55d6f99e82ae34589ad794fb6
Jalal Zainaddin <jalalzainaddin@gmail.com>
2025-10-06 22:17:01 +0300

    10/6/2025: Adding locks to the name semaphore example

--- MultiprocessingTest.py
+++ MultiprocessingTest.py
@@ -1,56 +1,32 @@
 from threading import *
 import time
 
-class BankAccount():
-  def __init__(self, name, balance):
-    self.name = name
-    self.balance = balance
+s = Semaphore(2) # creating the semaphore object initialized with 2 threads allowed to access simultaneously.
+l = Lock()
+# create a lock object here
 
-  def __str__(self):
-    return self.name
+def wish(name,age):
+   s.acquire()  # acquire sempahore instance by the current thread
+   for i in range(3):
+       # acquire the lock here
+       l.acquire()
+       print("Hi",name)
+       time.sleep(2)
+       print("Your age is",age)
+       # release the lock here
+       l.release()
+   s.release() # release semaphore instance by the current thread
 
-# These accounts are our shared resources
-account1 = BankAccount("account1", 100)
-account2 = BankAccount("account2", 0)
-l = Lock() # creating the lock object
+t1=Thread(target=wish, args=("Abullah",15))
+t2=Thread(target=wish, args=("Muhammad",20))
+t3=Thread(target=wish, args=("Abubaker",25))
+t4=Thread(target=wish, args=("Omar",30))
 
-class BankTransferThread(Thread):
-  def __init__(self, sender, receiver, amount):
-    Thread.__init__(self)
-    self.sender = sender
-    self.receiver = receiver
-    self.amount = amount
-
-  def run(self):
-    # Adding Lock to fix the Race Condition
-    l.acquire()
-    sender_initial_balance = self.sender.balance
-    sender_initial_balance -= self.amount
-    # Inserting delay to allow switch between threads
-    time.sleep(0.001)
-    
-    self.sender.balance = sender_initial_balance
-
-    receiver_initial_balance = self.receiver.balance
-    receiver_initial_balance += self.amount
-    
-    # Inserting delay to allow switch between threads
-    time.sleep(0.001)
-    self.receiver.balance = receiver_initial_balance
-    l.release()
-
-if __name__ == "__main__":
-
-  threads = []
-
-  for i in range(100):
-    threads.append(BankTransferThread(account1, account2, 1))
-
-  for thread in threads:
-    thread.start()
-
-  for thread in threads:
-    thread.join()
-
-  print('account1 ', account1.balance)
-  print('account2 ', account2.balance)
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t1.join()
+t2.join()
+t3.join()
+t4.join()
